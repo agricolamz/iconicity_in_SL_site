253007a30ea7fc3f9d5c58adc39dfd1e654d9dbf
agricolamz <agricolamz@gmail.com>
2017-09-06 07:35:50 +0300

    add graph tab; add raw data tab (sic! @pandaklez, here is our iconicity repo)

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(DT)
 library(lingtypology)
+library(ggplot2)
 library(dplyr)
 library(markdown)
 library(leaflet)
@@ -8,7 +9,12 @@ library(leaflet)
 function(input, output) {
 # section for word search -------------------------------------------------
   database <- read.csv("iconisity_SL.csv", sep = "\t", stringsAsFactors = FALSE)
-  
+  output$full_table <- DT::renderDataTable(
+    database[, -c(5, 7)],
+    filter = 'top',
+    rownames = FALSE,
+    options = list(pageLength = 20, autoWidth = FALSE, dom = 'tip'),
+    escape = FALSE)
   output$word_map <- renderLeaflet({
   loc <- ifelse(input$loc == "yes", "1", "[^1]")
   if(input$loc == "all"){loc <- "[01-]"}
@@ -65,4 +71,27 @@ function(input, output) {
                 label = paste0(database$language, " (", database_count$n, ")"),
                 map.orientation = "Atlantic",
                 radius = database_count$n*5)})
+
+# section for graphs ------------------------------------------------------
+  output$graph_picture <- renderPlot({
+    database %>%
+      filter(semantic.field %in% input$graph_field) %>% 
+      count(word, languages) %>%
+      left_join(database) %>%
+      mutate(pattern = ifelse(grepl(input$iconicity_pattern_graph, form.image.assocaition.pattern),
+                              paste(input$iconicity_pattern_graph),
+                              "other types")) ->
+      database
+    output$graph_table <- DT::renderDataTable(
+      database[,c(1, 2, 6)],
+      filter = 'top',
+      rownames = FALSE,
+      options = list(pageLength = 7, autoWidth = FALSE, dom = 'tip'),
+      escape = FALSE)
+    database %>% 
+      ggplot(aes(languages, n, fill = pattern))+
+      geom_bar(stat = "identity", position = "dodge")+
+      coord_flip()+
+      theme_bw()
+    })
   }
